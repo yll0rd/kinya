403a6f74a3672c9c07e028795f3c11f9b77c6535
yll0rd <leoyoumbi83@gmail.com>
2025-04-11 15:27:18 +0200

    feat: integrate HlmCard components and directives for enhanced UI structure

--- src/app/components/home/continue-learning/continue-learning.component.ts
+++ src/app/components/home/continue-learning/continue-learning.component.ts
@@ -2,10 +2,27 @@ import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { ButtonModule } from 'primeng/button';
 import { CustomButtonComponent } from '../../shared/ui/button/button.component';
+import {
+  HlmCardContentDirective,
+  HlmCardDescriptionDirective,
+  HlmCardDirective,
+  HlmCardFooterDirective,
+  HlmCardHeaderDirective,
+  HlmCardTitleDirective,
+} from '@spartan-ng/ui-card-helm';
 
 @Component({
   selector: 'app-continue-learning',
-  imports: [CustomButtonComponent, RouterLink],
+  imports: [
+    CustomButtonComponent,
+    RouterLink,
+    HlmCardContentDirective,
+    HlmCardDescriptionDirective,
+    HlmCardDirective,
+    HlmCardFooterDirective,
+    HlmCardHeaderDirective,
+    HlmCardTitleDirective,
+  ],
   templateUrl: './continue-learning.component.html',
   styleUrl: './continue-learning.component.css'
 })
