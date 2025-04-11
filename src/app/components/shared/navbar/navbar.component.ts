480c5ddf460e8711156995954f9b8a499aa7e8a9
yll0rd <leoyoumbi83@gmail.com>
2025-04-11 17:46:10 +0200

    Refactor code structure for improved readability and maintainability

--- src/app/components/shared/navbar/navbar.component.ts
+++ src/app/components/shared/navbar/navbar.component.ts
@@ -3,10 +3,25 @@ import { Component, signal } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { ButtonModule } from 'primeng/button';
 import { ThemeToggleComponent } from './theme-toggle/theme-toggle.component';
+import { BrnSheetContentDirective, BrnSheetTriggerDirective } from '@spartan-ng/brain/sheet';
+import {
+  HlmSheetComponent,
+  HlmSheetContentComponent,
+} from '@spartan-ng/ui-sheet-helm';
+import { HlmButtonDirective } from '@spartan-ng/ui-button-helm';
 
 @Component({
   selector: 'app-navbar',
   imports: [
+
+    HlmSheetComponent,
+    HlmSheetContentComponent,
+
+    BrnSheetContentDirective,
+    BrnSheetTriggerDirective,
+
+    HlmButtonDirective,
+
     RouterLink,
     NgFor,
     // NgIf,
@@ -22,14 +37,18 @@ export class NavbarComponent {
     {
       name: "Lessons",
       href: "/lessons",
+      icon: "pi pi-book",
+
     },
     {
       name: "Profile",
       href: "/profile",
+      icon: "pi pi-user",
     },
     {
       name: "Chat",
       href: "#",
+      icon: "pi pi-comment",
       disabled: true,
     },
   ])
