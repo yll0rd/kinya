a6f86dcf60b7f5514ab3f834dc508c1fa69f3d83
yll0rd <leoyoumbi83@gmail.com>
2025-04-13 12:41:21 +0200

    feat: replace custom button components with hlmBtn directive for consistency and improved UI

--- src/app/components/home/continue-learning/continue-learning.component.ts
+++ src/app/components/home/continue-learning/continue-learning.component.ts
@@ -1,15 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { RouterLink } from '@angular/router';
-import { CustomButtonComponent } from '../../shared/ui/button/button.component';
 import {
   HlmCardContentDirective,
   HlmCardDirective,
 } from '@spartan-ng/ui-card-helm';
+import { HlmButtonDirective } from '@spartan-ng/ui-button-helm';
 
 @Component({
   selector: 'app-continue-learning',
   imports: [
-    CustomButtonComponent,
+    HlmButtonDirective,
     RouterLink,
     HlmCardContentDirective,
     HlmCardDirective,
@@ -18,4 +18,12 @@ import {
   styleUrl: './continue-learning.component.css'
 })
 export class ContinueLearningComponent {
+  // Mock data for continue learning
+  @Input() continueLesson = {
+    id: 1,
+    title: "Common Greetings",
+    category: "Greetings",
+    progress: 65,
+    lastAccessed: "2 days ago",
+  }  
 }
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,13 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
-import { ButtonModule } from 'primeng/button';
 import { YourProgessComponent } from '../../components/home/your-progess/your-progess.component';
 import { ContinueLearningComponent } from '../../components/home/continue-learning/continue-learning.component';
 import { RecommendedLessonsComponent } from '../../components/home/recommended-lessons/recommended-lessons.component';
+import { HlmButtonDirective } from '@spartan-ng/ui-button-helm';
 
 @Component({
   selector: 'app-home',
-  imports: [RouterLink, YourProgessComponent, ContinueLearningComponent, RecommendedLessonsComponent, ButtonModule],
+  imports: [RouterLink, YourProgessComponent, ContinueLearningComponent, RecommendedLessonsComponent, HlmButtonDirective],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
